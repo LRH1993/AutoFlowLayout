0615770f435e2b22a61ccc00775d3de03301d43d
lvruheng <lvruheng@bytedance.com>
2017-07-27 17:58:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lvruheng/autoflowlayout/AutoFlowLayout.java
@@ -0,0 +1,209 @@
+package com.example.lvruheng.autoflowlayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *自定义LinearLayout，支持自动换行，指定行数,实现流式布局
+ */
+public class AutoFlowLayout extends LinearLayout  {
+    /**
+     * 存储所有的View，按行记录
+     */
+    private List<List<View>> mAllViews = new ArrayList<List<View>>();
+    private Context mContext;
+    private LayoutInflater mLayoutInflater;
+
+    /**
+     * 记录每一行的最大高度
+     */
+    private List<Integer> mLineHeight = new ArrayList<Integer>();
+    private String mCategoryName;
+
+    public AutoFlowLayout(Context context) {
+        super(context);
+        mContext = context;
+        init();
+    }
+
+    public AutoFlowLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+        init();
+    }
+
+    public AutoFlowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mContext = context;
+        init();
+    }
+
+    private void init() {
+        mLayoutInflater = LayoutInflater.from(getContext());
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        // 获得它的父容器为它设置的测量模式和大小
+        int sizeWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
+        int modeWidth = MeasureSpec.getMode(widthMeasureSpec);
+        int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
+        // 如果是warp_content情况下，记录宽和高
+        int width = 0;
+        int height = 0;
+        /**
+         * 记录每一行的宽度，width不断取最大宽度
+         */
+        int lineWidth = getPaddingLeft() + getPaddingRight();
+        /**
+         * 每一行的高度，累加至height
+         */
+        int lineHeight = getPaddingTop() + getPaddingBottom();
+
+        int cCount = getChildCount();
+
+        // 遍历每个子元素
+        for (int i = 0; i < cCount; i++) {
+            View child = getChildAt(i);
+            // 测量每一个child的宽和高
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+            // 得到child的lp
+            MarginLayoutParams lp = (MarginLayoutParams) child
+                    .getLayoutParams();
+            // 当前子空间实际占据的宽度
+            int childWidth = child.getMeasuredWidth() + lp.leftMargin
+                    + lp.rightMargin;
+            // 当前子空间实际占据的高度
+            int childHeight = child.getMeasuredHeight() + lp.topMargin
+                    + lp.bottomMargin;
+            /**
+             * 如果加入当前child，则超出最大宽度，则的到目前最大宽度给width，类加height 然后开启新行
+             */
+            if (lineWidth + childWidth > sizeWidth) {
+                width = Math.max(lineWidth, childWidth);// 取最大的
+                lineWidth = childWidth; // 重新开启新行，开始记录
+                // 叠加当前高度，
+                height += lineHeight;
+                // 开启记录下一行的高度
+                lineHeight = childHeight;
+                break;
+            } else
+            // 否则累加值lineWidth,lineHeight取最大高度
+            {
+                lineWidth += childWidth;
+                lineHeight = Math.max(lineHeight, childHeight);
+            }
+            // 如果是最后一个，则将当前记录的最大宽度和当前lineWidth做比较
+            if (i == cCount - 1) {
+                width = Math.max(width, lineWidth);
+                height += lineHeight;
+            }
+
+        }
+        setMeasuredDimension((modeWidth == MeasureSpec.EXACTLY) ? sizeWidth
+                : width, (modeHeight == MeasureSpec.EXACTLY) ? sizeHeight
+                : height);
+    }
+
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        mAllViews.clear();
+        mLineHeight.clear();
+
+        int width = getWidth();
+
+        int lineWidth = getPaddingLeft();
+        int lineHeight = getPaddingTop();
+        // 存储每一行所有的childView
+        List<View> lineViews = new ArrayList<View>();
+        int cCount = getChildCount();
+        // 遍历所有的孩子
+        for (int i = 0; i < cCount; i++) {
+            View child = getChildAt(i);
+            MarginLayoutParams lp = (MarginLayoutParams) child
+                    .getLayoutParams();
+            int childWidth = child.getMeasuredWidth();
+            int childHeight = child.getMeasuredHeight();
+
+            // 如果已经需要换行
+            if (childWidth + lp.leftMargin + lp.rightMargin + getPaddingRight() + lineWidth > width) {
+                // 记录这一行所有的View以及最大高度
+                mLineHeight.add(lineHeight);
+                // 将当前行的childView保存，然后开启新的ArrayList保存下一行的childView
+                mAllViews.add(lineViews);
+                lineWidth = 0;// 重置行宽
+                lineViews = new ArrayList<View>();
+                break;
+            }
+            /**
+             * 如果不需要换行，则累加
+             */
+            lineWidth += childWidth + lp.leftMargin + lp.rightMargin;
+            lineHeight = Math.max(lineHeight, childHeight + lp.topMargin
+                    + lp.bottomMargin);
+            lineViews.add(child);
+        }
+        // 记录最后一行
+        mLineHeight.add(lineHeight);
+        mAllViews.add(lineViews);
+
+        int left = getPaddingLeft();
+        int top = getPaddingTop();
+        // 得到总行数
+        int lineNums = mAllViews.size();
+        for (int i = 0; i < lineNums; i++) {
+            // 每一行的所有的views
+            lineViews = mAllViews.get(i);
+            // 当前行的最大高度
+            lineHeight = mLineHeight.get(i);
+
+            Log.e("SingleLineLinerLayout", "第" + i + "行 ：" + lineViews.size() + " , " + lineViews);
+            Log.e("SingleLineLinerLayout", "第" + i + "行， ：" + lineHeight);
+
+            // 遍历当前行所有的View
+            for (int j = 0; j < lineViews.size(); j++) {
+                View child = lineViews.get(j);
+                if (child.getVisibility() == View.GONE) {
+                    continue;
+                }
+                MarginLayoutParams lp = (MarginLayoutParams) child
+                        .getLayoutParams();
+
+                //计算childView的left,top,right,bottom
+                int lc = left + lp.leftMargin;
+                int tc = top + lp.topMargin;
+                int rc = lc + child.getMeasuredWidth();
+                int bc = tc + child.getMeasuredHeight();
+
+                Log.e("SingleLineLinerLayout", child + " , l = " + lc + " , t = " + t + " , r ="
+                        + rc + " , b = " + bc);
+
+                child.layout(lc, tc, rc, bc);
+
+                left += child.getMeasuredWidth() + lp.rightMargin
+                        + lp.leftMargin;
+            }
+            left = getPaddingLeft();
+            top += lineHeight;
+        }
+    }
+
+    public void setAllViews(List<View> views) {
+        removeAllViews();
+        if (views == null || views.size() == 0) {
+            return;
+        }
+        for (int i = 0; i < views.size(); i++) {
+            addView(views.get(i));
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lvruheng/autoflowlayout/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.lvruheng.autoflowlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
